0310b90c377aa416052c8555044d57880c722c6c
lucasEngdComp <48529983+lucasEngdComp@users.noreply.github.com>
2022-12-15 16:39:14 -0300

    Add files via upload

--- teste_de_software-main/TesteFuncionalidades.java
+++ teste_de_software-main/TesteFuncionalidades.java
@@ -10,9 +10,8 @@ class TesteFuncionalidades {
 	
 	public Sistema  sistema = new Sistema();
 	
-	
-	
 	// REQUISTO FUNCIONAL 1 - CADASTRO DE USUÁRIO
+	
 	@Test()
 	public void validaUsuario() {
 		// cadastro inválido
@@ -22,6 +21,9 @@ class TesteFuncionalidades {
 		assertNull(sistema.cadastraUsuario("Ialy Sousa", "ialysousa@gmail", "ndndndndnd"));
 		
 	}
+	
+	// REQUISTO FUNCIONAL 1 - CADASTRO DE USUÁRIO
+	
 	@Test()
 	public void criacaoDoUsuario() {
 		//Cadastro válido
@@ -29,6 +31,7 @@ class TesteFuncionalidades {
 	}
 	
 	// REQUISITO FUNCIONAL 2 - FUNÇÃO DO SEGUNDO GRAU
+	
 	@Test()
 	public void validaFuncaoSegundoGrau() {
 		// Não possui raiz
@@ -60,6 +63,8 @@ class TesteFuncionalidades {
         assertTrue(sistema.ehTriangulo(1, 79, 100)); // É TRIANGULO
 	}
 	
+	//REQUISITO FUNCIONAL 4 - CLASSIFICAÇÃO 
+	
 	@Test()
 	public void classificaTriangulo() {
 		var mensagemDeErro = "sem classificação";
@@ -67,12 +72,15 @@ class TesteFuncionalidades {
 		assertEquals(sistema.classificaTriangulo(180, 180, 180), mensagemDeErro);
 		assertEquals(sistema.classificaTriangulo(-1, -1, -1), mensagemDeErro);
 		
-		assertTrue(sistema.classificaTriangulo(90, 45, 45).contains("retângulo"));
-        assertTrue(sistema.classificaTriangulo(70, 70, 40).contains("acutângulo"));
-        assertTrue(sistema.classificaTriangulo(100, 40, 40).contains("obtusângulo"));
+		assertTrue(sistema.classificaTriangulo(90, 45, 45).contains("Retangulo"));
+        assertTrue(sistema.classificaTriangulo(70, 70, 40).contains("Acutangulo"));
+        assertTrue(sistema.classificaTriangulo(100, 40, 40).contains("Obtusângulo"));
+        
+     
 		
 		
 	}
+	//REQUISITO FUNCIONAL 5 - É TRIÂNGULO RETÂNGULO
 	
 	@Test()
     public void ehTrianguloRetangulo() {
@@ -81,23 +89,32 @@ class TesteFuncionalidades {
         assertTrue(sistema.ehTrianguloRetangulo(10, 8, 6));
 	}
 	
+	
+	//REQUISITO FUNCIONAL 6 - É RETÂNGULO
+	
 	@Test()
     public void ehRetangulo() {
         assertTrue(sistema.ehRetangulo(90, 90, 90, 90));
         assertFalse(sistema.ehRetangulo(0, -10, 1233123123, 331293));
 	}
 	
+	//REQUISITO FUNCIONAL - 7 - PERÍMETRO
+	
     @Test()
     public void perimetroRetangulo() {
         assertNotNull(sistema.perimetroRetangulo(200, 19));
         assertNotNull(sistema.perimetroRetangulo(-100, -10000.123));
     }
+    
+    //REQUISITO FUNCIONAL - 7 - ÁREA
 
     @Test()
     public void areaRetangulo() {
         assertNotNull(sistema.areaRetangulo(12, 12));
         assertNotNull(sistema.areaRetangulo(-10, -1000));
     }
+    
+    //REQUISITO FUNCIONAL CALCULADORA
 
     @Test()
     public void validacaoCalculadora() {
@@ -110,9 +127,12 @@ class TesteFuncionalidades {
             assertEquals(sistema.calculadora("1000000000000000000000000 + 1"), "1000000000000000000000001.00");
         });
     }
-
+    
+    //REQUISITO FUNCIONAL CALCULADORA
+    
     @Test()
     public void calculadora() {
+    	
         assertEquals(sistema.calculadora("1 + 1"), "2");
         assertEquals(sistema.calculadora("1 / 0"),"∞");
         assertEquals(sistema.calculadora("1 + 0"), "1.00");
@@ -124,19 +144,25 @@ class TesteFuncionalidades {
         assertEquals(sistema.calculadora("2 ** 2"), "4.00");
         assertEquals(sistema.calculadora("1000000000 - 1"), "999999999.00");
     }
-
+    
+    //REQUISITO FUNCIONAL 9 - ÁREA
+    
     @Test
     public void areaCirculo() {
         assertEquals(sistema.areaCirculo(10), 314.1592653589793);
         assertEquals(sistema.areaCirculo(0), 0);
     }
 
+    //REQUISITO FUNCIONAL 9 - PERÍMETRO
+    
     @Test
     public void perimetroCirculo() {
         // talvez truncar o resultado em menos casas decimais
         assertEquals(sistema.perimetroCirculo(10), 62.83185307179586);
         assertEquals(sistema.perimetroCirculo(0), 0);
     }
+    
+    //REQUISITO FUNCIONAL 10 - DISTÂNCIA ENTRE DOIS PONTOS
 
     @Test
     public void distanciaEntreDoisPontos() {
